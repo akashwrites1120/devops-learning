f51942c079438eca54e200de7cb854023dd85a4a
Akash Maity <akashmaity4452@gmail.com>
2025-12-21 01:12:36 +0530

    day 3 variable.sh

---
+++ day-03-linux/variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name="akash"
+learning="devops"
+echo "My name is $name and I am currently learning $devops"
+
+echo "We are using $SHELL"
+
+echo "We are at $HOME and today is " && date
